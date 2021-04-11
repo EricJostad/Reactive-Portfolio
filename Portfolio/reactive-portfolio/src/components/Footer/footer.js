157470da18cb8f736b5e274ea498bf67c883bc97
EJostad <eric.jostad@outlook.com>
2021-04-10 19:24:17 -0700

    Added initial code to footer.js file. Also commented out import of react-bootstrap as there is a bug that is not yet squashed.

---
+++ Portfolio/reactive-portfolio/src/components/Footer/footer.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Card } from 'react-bootstrap';
+import '../Footer/footer.css';
+
+function Footer() {
+    return (
+        <>
+        <br></br>
+        <div className='footer'>
+            <Card id='foot-card' style={{ width: '18rem' }}>
+            </Card>
+        </div>
+        </>
+    );
+}
+
+export default Footer;
