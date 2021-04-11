a7113194c34b0c300e9446ad6759caf423c913e8
EJostad <eric.jostad@outlook.com>
2021-04-10 19:20:48 -0700

    Added initial code to About.js file as well as installed react-bootstrap.

---
+++ Portfolio/reactive-portfolio/src/components/About/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../About/about.css';
+import { Jumbotron, } from 'react-bootstrap';
+
+function About() {
+    return (
+        <Jumbotron className='bio-jumbo'>
+        </Jumbotron>
+    );
+}
+
+export default About;
