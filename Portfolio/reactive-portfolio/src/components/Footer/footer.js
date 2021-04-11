e90469fdfeec06b6293769bac006fe3e97a3f33d
EJostad <eric.jostad@outlook.com>
2021-04-11 00:29:44 -0700

    Added contact form section topersistent Footer.

--- Portfolio/reactive-portfolio/src/components/Footer/footer.js
+++ Portfolio/reactive-portfolio/src/components/Footer/footer.js
@@ -1,16 +1,24 @@
 import React from 'react';
-import { Card } from 'react-bootstrap';
+import { Form } from 'react-bootstrap';
 import '../Footer/footer.css';
 
 function Footer() {
     return (
-        <>
-        <br></br>
-        <div className='footer'>
-            <Card id='foot-card' style={{ width: '18rem' }}>
-            </Card>
-        </div>
-        </>
+        <Form>
+        <Form.Group>
+          <label for="name" class="col-form-label">What is your name?</label>
+          <input type="text" class="form-control" placeholder="Name" id="name"/>
+        </Form.Group>
+        <Form.Group>
+          <label for="email" class="col-form-label">What is your email?</label>
+          <input type="email" class="form-control" placeholder="Email" id="email"/>
+        </Form.Group>
+        <Form.Group>
+          <label for="message" class="col-form-label">Ask me something</label>
+          <textarea class="form-control" placeholder="Message" id="message"></textarea>
+        </Form.Group>
+        <button type="submit" class="btn btn-secondary">Submit</button>
+      </Form>
     );
 }
 
